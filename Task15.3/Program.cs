b9d29fd975f37df1d18e2dda68d02422d2e69b54
Alexander Nuykin <scepticnav@gmail.com>
2022-05-18 16:04:27 +0300

    Add Task 15.3

---
+++ Task15.3/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 15 (С использованием "Random"): Напишите программу, которая проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+int DayNumber = new Random().Next(1,8);
+Console.WriteLine($"Сгенерированное число: {DayNumber}");
+
+if (DayNumber > 5)
+{
+    Console.WriteLine("Да");
+}
+
+else
+{
+    Console.WriteLine("Нет");
+}
